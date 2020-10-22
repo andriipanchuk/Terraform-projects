345659605b4d71322cb2ea35a21bc93a368002a7
Safa Gumuscibik <safagumus05@gmail.com>
2020-10-22 12:35:20 -0400

    added a new folder for cloud front

---
+++ cloudfront-static-web-aws/route53.tf
@@ -0,0 +1,16 @@
+# # Only need below if you do not have a route53 zone
+# resource "aws_route53_zone" "primary" {
+#   name = var.domain_name
+# }
+
+#Below will create you an A record that ties to your bucket that will be created with s3.tf file
+resource "aws_route53_record" "www" {
+  zone_id = var.route_zone_id  #pulls your route53 zone ID from tfvars file
+  name = var.domain_name       #pulls your domain name from tfvars file
+  type = "A"
+  alias {
+    name = aws_s3_bucket.website_bucket.website_domain
+    zone_id = aws_s3_bucket.website_bucket.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
