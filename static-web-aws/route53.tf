ea641a4d1e5cad70538717bc911bb6cc59fa9c04
Safa Gumuscibik <safagumus05@gmail.com>
2020-10-21 21:30:35 -0400

    updated route53 zone ID

--- static-web-aws/route53.tf
+++ static-web-aws/route53.tf
@@ -3,7 +3,8 @@ resource "aws_route53_zone" "primary" {
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = var.route_zone_id
+  #zone_id = aws_route53_zone.primary.zone_id
   name = var.domain_name
   type = "A"
   alias {
--- static-web-aws/terraform.tfvars
+++ static-web-aws/terraform.tfvars
@@ -1,2 +1,3 @@
 domain_name = "test123.safa-g.com"
 aws_region = "us-east-1"
+route_zone_id = "Z3JDKR1YW6FM9I"
