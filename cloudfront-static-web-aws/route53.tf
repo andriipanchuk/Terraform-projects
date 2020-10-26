3a986cd14cf86bc8bc619e6d4e2d9464c2ff7b9a
Safa Gumuscibik <safagumus05@gmail.com>
2020-10-26 16:38:02 -0400

    updated files

--- cloudfront-static-web-aws/route53.tf
+++ cloudfront-static-web-aws/route53.tf
@@ -9,8 +9,18 @@ resource "aws_route53_record" "www" {
   name = var.domain_name       #pulls your domain name from tfvars file
   type = "A"
   alias {
-    name = aws_s3_bucket.website_bucket.website_domain
-    zone_id = aws_s3_bucket.website_bucket.hosted_zone_id
+    name = aws_cloudfront_distribution.prod_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.prod_distribution.hosted_zone_id
     evaluate_target_health = false
   }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  count = length(aws_acm_certificate.cert.domain_validation_options)
+  name = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_name, count.index)
+  type = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_type, count.index)
+  zone_id = var.route_zone_id  #pulls your route53 zone ID from tfvars file
+  records = [element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_value, count.index)]
+  ttl = 60
+  depends_on = [aws_acm_certificate.cert]
 }
